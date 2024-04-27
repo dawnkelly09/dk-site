a109983a6b7c39afa5e85e515a0b1b589b77aef8
Dawn Kelly <dawnkelly09@gmail.com>
2024-04-27 13:22:28 -0400

    first commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+import DK from '../../public/DK.svg'
+import '../globals.css'
+
+const Header = () => {
+  return (
+    <nav>
+        <div className="nav-logo">
+            <Link href="/" passHref legacyBehavior>
+                <Image priority src={DK} alt="DK logo" height="80"/>
+            </Link>
+            <div>
+                <span className="first-name">DAWN </span><span className="last-name">KELLY</span>
+            </div>
+        </div>
+        <div>
+            <Link href="/about" className='links-row'>about</Link>
+            <Link href="/portfolio" className='links-row'>portfolio</Link>
+            <Link href="/contact" className='links-row'>contact</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
---
+++ app/layout.js
@@ -0,0 +1,35 @@
+import { Titillium_Web, Audiowide } from "next/font/google";
+import "./globals.css";
+import Header from "./components/Header";
+
+const tweb = Titillium_Web({ 
+  subsets: ["latin"],
+  variable: ['--font-text'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+});
+
+const audio = Audiowide({
+  subsets: ['latin'],
+  variable: ['--font-display'],
+  weight: '400',
+  style: 'normal',
+  display: 'swap',
+})
+
+export const metadata = {
+  title: "Dawn Kelly",
+  description: "Dawn Kelly - blockchain builder and developer advocate",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${tweb.variable} ${audio.variable}`}>
+      <body>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
