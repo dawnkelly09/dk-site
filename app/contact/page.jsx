7d0f3d91b84b60258ee20c8b510b587ef33471f9
Dawn Kelly <dawnkelly09@gmail.com>
2024-04-29 17:26:39 -0400

    adds placeholders to work and contact pages

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,26 @@
-
+import Image from 'next/image'
+import Matrix from '../../public/matrix.jpg'
 
 const ContactPage = () => {
   return (
-    <div>contact page</div>
+    <main>
+      <section>
+        <div className='center-me'>
+          <Image 
+            src={Matrix}
+            style={{
+              borderRadius: "12px",
+            }}
+          />
+        </div>
+      </section>
+      <section>
+        socials
+      </section>
+      <section>
+        resume
+      </section>
+    </main>
   )
 }
 
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
+import Matrix from '../../public/matrix.jpg'
 
 const WorkPage = () => {
   return (
-    <div>portfolio page</div>
+    <main>
+      <section>
+        <div className='center-me'>
+          <Image 
+            src={Matrix}
+            style={{
+              borderRadius: "12px",
+            }}
+          />
+        </div>
+      </section>
+      <section>
+        coding projects
+      </section>
+      <sectiom>
+        blogs 
+      </sectiom>
+      <section>
+        workshops
+      </section>
+    </main>
   )
 }
 
