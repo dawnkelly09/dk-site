d547dc015937e192c61ad3e53f79f6993ae84858
Dawn Kelly <dawnkelly09@gmail.com>
2024-04-30 11:49:07 -0400

    adds initial styling of footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,28 +2,30 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <section className="">
-        <div className="">
-            <h2 className=''>Dawn Kelly</h2>
-            <h2 className=''>dawnkelly09@gmail.com</h2>
-            <span className="">© Dawn Kelly - last updated April 30, 2024</span>
+    <section className="footer-container">
+        <div className="footer-left">
+            <h4>Dawn Kelly</h4>
+            <h5>dawnkelly09@gmail.com</h5>
+            <span className="small-text">© Dawn Kelly - last updated April 30, 2024</span>
         </div>
-        <div className="m-8 text-lg text-white">
-            <h4 id='contact' className='font-display color-ink'>Where to find me:</h4>
-            <Link href="https://twitter.com/run4pancakes" className="text-md mr-2 hover:text-aqua" target="_blank">
-                twitter |
-            </Link>
-            <Link href="https://www.linkedin.com/in/dawnkelly09/" className="text-md mr-2 hover:text-aqua" target="_blank">
-                linkedin |
-            </Link>
-            <Link href="https://t.me/dawnkelly" className="text-md mr-2 hover:text-aqua" target="_blank">
+        <div className="contact-links">
+            <h4 id='contact'>Where to find me:</h4>
+            <Link href="https://t.me/dawnkelly" className="contact-link" target="_blank" rel="noopener">
                 telegram |
             </Link>
-            <Link href="https://discordapp.com/users/dawnkelly09" className="text-md mr-2 hover:text-aqua" target="_blank">
-                discord
+            <Link href="https://twitter.com/run4pancakes" className="contact-link" target="_blank" rel="noopener">
+                twitter |
+            </Link>
+            <Link href="https://warpcast.com/dawnkelly" className="contact-link" target="_blank" rel="noopener">
+                farcaster |
+            </Link>
+            <Link href="https://discordapp.com/users/dawnkelly09" className="contact-link" target="_blank" rel="noopener">
+                discord |
+            </Link>
+            <Link href="https://www.linkedin.com/in/dawnkelly09/" className="contact-link" target="_blank" rel="noopener">
+                linkedin |
             </Link>
         </div>
-        
     </section>
   )
 }
